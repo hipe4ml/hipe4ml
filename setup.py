9c8e6f10ca6157762b12f47d32138efb7cb4286d
Maximiliano Puccio <mpuccio@users.noreply.github.com>
2019-10-29 23:54:22 +0100

    Set the current tag to dev0
    Next step would be having automatic tagging from the git repository as discussed with @mconcas

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ SETUP = Setup(
     name="hipe4ml",
 
     # LAST-TAG is a placeholder. Automatically replaced at deploy time with the right tag
-    version="0.0.1",
+    version="0.0.dev0",
 
     description="Minimal heavy ion physics environment for Machine Learning",
 
