4f3d05ecd26eac927a60aa6e520b48f2cd4fa509
Fabio Catalano <fabio.catalano@cern.ch>
2020-02-28 19:07:53 +0100

    Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ SETUP = Setup(
     name="hipe4ml",
 
     # LAST-TAG is a placeholder. Automatically replaced at deploy time with the right tag
-    version="0.0.3",
+    version="0.0.4",
 
     description="Minimal heavy ion physics environment for Machine Learning",
 
