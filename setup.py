1af69521c74dd428e80a78c85789d9cf11e9ee24
Luca Barioglio <luca.barioglio@cern.ch>
2020-03-07 15:40:55 +0100

    Exclude tutorials from package

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ SETUP = Setup(
 
     # You can just specify the packages manually here if your project is simple. Or you can use
     # find_packages().
-    packages=find_packages(),
+    packages=find_packages(exclude=['tutorials']),
 
     # List run-time dependencies here. These will be installed by pip when your project is
     # installed. For an analysis of "install_requires" vs pip's requirements files see:
