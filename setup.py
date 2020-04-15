effcf2e4b738800a289dcdf11abd2cbd483e6f3e
Pietro Fecchio <pfecchio@cern.ch>
2020-04-15 16:59:42 +0200

    fix unexpected behavior of installation from local source

--- setup.py
+++ setup.py
@@ -79,7 +79,8 @@ SETUP = Setup(
 
     # You can just specify the packages manually here if your project is simple. Or you can use
     # find_packages().
-    packages=find_packages(exclude=['tutorials']),
+    packages=find_packages(where='hipe4ml'),
+    package_dir={"": "hipe4ml"},
 
     # List run-time dependencies here. These will be installed by pip when your project is
     # installed. For an analysis of "install_requires" vs pip's requirements files see:
