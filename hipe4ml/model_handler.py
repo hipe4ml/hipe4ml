343d13d65dd02a2dca238c54b7c75507d545f11d
Francesco Mazzaschi <fmazzasc@cern.ch>
2020-05-27 09:11:54 +0200

    Add n_classes info when loading ModelHandlers

--- hipe4ml/model_handler.py
+++ hipe4ml/model_handler.py
@@ -424,3 +424,4 @@ class ModelHandler:
         self.training_columns = loaded_model.get_training_columns()
         self.model_params = loaded_model.get_model_params()
         self.model_string = loaded_model.get_model_module()
+        self._n_classes = loaded_model.get_n_classes()
