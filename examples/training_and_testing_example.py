320b160bd1f3999637cc81d7feb2152f323d78f4
luca-bottero <lucabottero99@gmail.com>
2022-01-10 16:55:10 +0100

    Update

--- examples/training_and_testing_example.py
+++ examples/training_and_testing_example.py
@@ -35,6 +35,11 @@ HYP_RANGES = {
     # 'learning_rate': (0.01, 0.3),  # learning rate
     'n_estimators': (5, 10),  # number of boosting trees
 }
+
+# hyperparams optimization with Optuna
+MODEL.optimize_params_optuna(DATA, HYP_RANGES, scoring='roc_auc', direction='maximize', timeout = 10, n_trials = None)
+
+# Bayesian hypermaprameter optimization
 MODEL.optimize_params_bayes(DATA, HYP_RANGES, 'roc_auc')
 
 # train and test the model with the updated hyperparameters
