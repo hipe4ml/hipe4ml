7521a611f786971e5f22cbf4b669849928382638
Maximiliano Puccio <maximiliano.puccio@cern.ch>
2025-10-23 09:56:34 +0200

    Bump to 0.0.21

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ SETUP = Setup(
     name="hipe4ml",
 
     # LAST-TAG is a placeholder. Automatically replaced at deploy time with the right tag
-    version="0.0.20",
+    version="0.0.21",
 
     description="Minimal heavy ion physics environment for Machine Learning",
 
